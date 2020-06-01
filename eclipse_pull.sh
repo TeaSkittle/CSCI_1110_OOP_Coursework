fdafbf09dc6332a8ca3a2e905dcfd1a2bca64b8d
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-06-01 11:52:01 -0600

    Update eclipse_pull.sh

--- eclipse_pull.sh
+++ eclipse_pull.sh
@@ -4,9 +4,17 @@
 # Usage: eclipse_pull.sh chapter project_name
 #    ex: sh eclipse_pull.sh 11 Ex11_3
 
+echo "[+] Pulling java files from eclipse folder.."
 if [ ! -f "/Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/" ]; then
     mkdir /Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/
     cp /Users/student/Documents/eclipse-workspace/$2/$2/*java /Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/
 else
     cp /Users/student/Documents/eclipse-workspace/$2/$2/*java /Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/
 fi
+echo "[+] File pull complete"
+echo "[+] Moving to project folder.."
+cd /Users/student/Desktop/CSCI_1110_OOP_Coursework/
+pwd
+echo "[+] Checking git status.."
+git status
+echo "[-] Need to add, commit, and pull to finalize..."
