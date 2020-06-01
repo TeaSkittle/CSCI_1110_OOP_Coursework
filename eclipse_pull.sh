a0e51ccc4e1e2e05a8012c6929b053b5f284158e
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-06-01 13:58:49 -0600

    Update eclipse_pull.sh

--- eclipse_pull.sh
+++ eclipse_pull.sh
@@ -6,14 +6,14 @@
 
 echo "[+] Pulling java files from eclipse folder.."
 if [ ! -f "/Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/" ]; then
-    mkdir /Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/
-    cp /Users/student/Documents/eclipse-workspace/$2/$2/*java /Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/
+    mkdir "/Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/"
+    cp "/Users/student/Documents/eclipse-workspace/$2/$2/*java" "/Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/"
 else
-    cp /Users/student/Documents/eclipse-workspace/$2/$2/*java /Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/
+    cp "/Users/student/Documents/eclipse-workspace/$2/$2/*java" "/Users/student/Desktop/CSCI_1110_OOP_Coursework/Chapter$1/$2/"
 fi
 echo "[+] File pull complete"
 echo "[+] Moving to project folder.."
-cd /Users/student/Desktop/CSCI_1110_OOP_Coursework/
+cd /Users/student/Desktop/CSCI_1110_OOP_Coursework/ || { echo "[!] Error: directory not found"; exit 1; }
 pwd
 echo "[+] Checking git status.."
 git status
