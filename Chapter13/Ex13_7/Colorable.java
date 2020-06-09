04f827b93d702f3abb1f31bc67fca9c31e1d639b
404kevnotfound <student@HSiMac4-101.local>
2020-06-09 11:49:15 -0600

    started ex13-7

---
+++ Chapter13/Ex13_7/Colorable.java
@@ -0,0 +1,12 @@
+// (c) Travis Dowd
+// (d) 6-9-2020
+//
+// Chapter 13, exercise 13-7
+
+package Ex13_7;
+
+public interface Colorable {
+	public void howToColor() {
+		System.out.println( "Color all three sides" );
+	}
+}
---
+++ Chapter13/Ex13_7/test.java
@@ -0,0 +1,17 @@
+// (c) Travis Dowd
+// (d) 6-9-2020
+//
+// Chapter 13, exercise 13-7
+
+package Ex13_7;
+
+import java.util.Scanner;
+
+class test {
+	public static void main( String[] args ) {
+		// Think these need to be Triangle( s )
+		// or GeometricObject isn't supposed to be abstract
+		GeometricObject[] obj = new GeometricObject[ 5 ];
+		obj[ 1 ] = new GeometricObject();
+	}
+}
