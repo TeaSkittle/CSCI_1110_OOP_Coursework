9891af1fc2518dd08bd98217cc3d7d2aed5bb6b7
404kevnotfound <student@HSiMac4-9.local>
2020-06-24 12:31:06 -0600

    Upload Ex15_3

---
+++ Chapter15/Ex15_3/BallPane.java
@@ -0,0 +1,41 @@
+// (c) Travis Dowd
+// (d) 6-24-2020
+//
+// Chapter 15, exercise 15
+
+package Ex15;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+public class BallPane extends Pane {
+    public final double radius = 25;
+    Circle circle = new Circle( 125, 125, radius );
+    public BallPane() { getChildren().add( circle ); }
+    public double getRadius(){ return this.radius; }
+    public double getX() { return circle.getCenterX(); }
+    public double getY() { return circle.getCenterY(); }
+    public void setX( double x ) { circle.setCenterX( x ); }
+    public void setY( double y ) { circle.setCenterY( y ); }
+    // Move ball
+    public void moveLeft() {
+        if ( getX() > getRadius() || getX() > getWidth() - getRadius() ) {
+            setX( getX() - 5 );
+        }
+    }
+    public void moveRight() {
+        if( getX() < getRadius() || getX() < getWidth() - getRadius() ) {
+            setX( getX() + 5 );
+        }
+    }
+    public void moveUp() {
+        if ( getY() > getRadius() || getY() > getHeight() - getRadius() ) {
+            setY( getY() - 5 );
+        }
+    }
+    public void moveDown() {
+        if ( getY() < getRadius() || getY() < getHeight() - getRadius() ) {
+            setY( getY() + 5 );
+        }
+    }
+}
---
+++ Chapter15/Ex15_3/Main.java
@@ -0,0 +1,76 @@
+// (c) Travis Dowd
+// (d) 6-24-2020
+//
+// Chapter 15, exercise 15
+
+package Ex15;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+
+public class Main extends Application {
+    @Override
+    public void start( Stage primaryStage ) throws Exception {
+
+        // ----------------------
+        //   Create the objects
+        // ----------------------
+        // Buttons
+        HBox hbox = new HBox();
+        hbox.setSpacing( 10 );
+        hbox.setAlignment( Pos.BOTTOM_CENTER );
+        Button btUp = new Button( "Up" );
+        Button btDown = new Button( "Down" );
+        Button btLeft = new Button( "Left" );
+        Button btRight = new Button( "Right" );
+        hbox.getChildren().setAll( btUp, btDown, btLeft, btRight );
+        // Ball
+        BallPane ball = new BallPane();
+        BorderPane pane = new BorderPane();
+        pane.setCenter( ball );
+        pane.setBottom( hbox );
+
+        // ------------------
+        //   Move the ball
+        // ------------------
+        // Arrow Keys
+        ball.setOnKeyPressed( e -> {
+            switch ( e.getCode() ) {
+                case UP: ball.moveUp(); break;
+                case DOWN: ball.moveDown(); break;
+                case LEFT: ball.moveLeft(); break;
+                case RIGHT: ball.moveRight(); break;
+                default:
+            }
+        });
+        // Mouse buttons
+        btUp.setOnAction( new EventHandler<ActionEvent>() {
+            @Override public void handle( ActionEvent event ) { ball.moveUp(); }});
+        btDown.setOnAction( new EventHandler<ActionEvent>()  {
+            @Override public void handle( ActionEvent event ) { ball.moveDown(); }});
+        btLeft.setOnAction( new EventHandler<ActionEvent>()  {
+            @Override public void handle( ActionEvent event ) { ball.moveLeft(); }});
+        btRight.setOnAction( new EventHandler<ActionEvent>() {
+            @Override public void handle( ActionEvent event ) { ball.moveRight(); }});
+
+        // ----------------
+        //   Start scene
+        // ----------------
+        // Create scene and place in stage
+        Scene scene = new Scene( pane, 250, 250 );
+        primaryStage.setTitle( "Exercise 15" );
+        primaryStage.setScene( scene );
+        primaryStage.show();
+        ball.requestFocus();
+    }
+    public static void main( String[] args ) {
+        launch( args );
+    }
+}
