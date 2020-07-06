e30e82b151f3f2f45fe76369754920ac8d6567c1
404kevnotfound <student@HSiMac4-42.local>
2020-07-06 11:38:48 -0600

    added final/readme

--- Final/Calc.java
+++ Final/Calc.java
@@ -6,29 +6,31 @@
 // based off of: https://www.programcreek.com/2012/12/leetcode-evaluate-reverse-polish-notation/
 // overview of stack based calc: https://medium.com/@eauditory3/what-are-stack-based-calculators-cf2dbe249264
 //
-// TODO: - Add constructor methods ( to use in another project )
-//       - Add ability to call as one cammonds, and not use repl. Ex- Calc( "2 3 * 2 ^" );
-//       - Add factorial operator, ex - 5 ! = 120 ( https://www.programiz.com/c-programming/examples/factorial )
-//           - May have issues because it only uses one number ( maybe just push one number to stack )
-//
+// TODO: - Add ability to call as one commands, and not use repl. Ex- Calc( "2 3 * 2 ^" );
+//       
+
+package Final;
 
 import java.util.Stack;
 import java.lang.Math;
 import java.util.Scanner;
 
 public class Calc {
-	// Constructors
+	// Constructor
 	public Calc(){}
-	public Calc( String[] tokens ){
-		System.out.println( rpn( tokens ));
+	
+	// Caluclate an expression without the need for REPL loop
+	public double run( String[] tokens ){
+		return rpn( tokens );
 	}
+	// Loop and get input from user, use intereactively
 	public static void repl() {
 		int run = 1;
 		try ( Scanner input = new Scanner( System.in )) {
 			while ( run > 0 ) {
 				System.out.print( "> " );
 				String[] tokens = input.nextLine().split( " " );
-				if ( tokens[0] == "q" ) {
+				if ( tokens[0] == "q" || tokens[0] == "quit" ) {
 					System.out.println( "pass" );
 					run = 0;
 				} else {
@@ -36,13 +38,13 @@ public class Calc {
 				}
 			}
 		} catch ( Exception e ) {
-			run = 0;
+			//run = 0;
 			System.out.println( "error" );
 		} 
 	}
 	public static double rpn( String[] tokens ) throws java.lang.NumberFormatException {
 		double returnValue = 0;
-		String operators = "+-*/^%";
+		String operators = "+-*/^%!";
 		Stack<String> stack = new Stack<String>();
 		for( String t : tokens ){
 			if( !operators.contains( t )){
@@ -58,10 +60,16 @@ public class Calc {
 					case 3: stack.push( String.valueOf( b / a )); break;
 					case 4: stack.push( String.valueOf(( Math.pow( b, a )))); break;
 					case 5: stack.push( String.valueOf( b % a )); break;
+					case 6: 
+						double fact = 1;
+						stack.push( String.valueOf( b ));
+						for( int i = 1; i < a; i++){
+							fact *= i;
+						} stack.push( String.valueOf( fact ));
+						break;
 				}
 			}
-		}
-		returnValue =  Double.valueOf( stack.pop() );
+		} returnValue =  Double.valueOf( stack.pop() );
 		stack.clear();
 		return returnValue;
 	}
---
+++ Final/README.md
@@ -0,0 +1,96 @@
+# Final
+
+## Calc
+
+This calculator is stack based and uses Reverse Polish Notation( RPN or Postfix notation ). It can do all sorts of calculations, but the syntax is different from what we are normally use to. For example in normal syntax ( infix ), a simple addition problem looks as such:
+```
+12 + 7 = 17
+```
+With RPN that is done like this:
+```
+12 7 + = 17
+```
+
+It takes some time to get used to but you can do complex calcutaions even without the need to use parenthesis. A good example of its capabalities is converting Celcius to Fahrenheit. Here is an example converting 20°C to 68°F:
+```
+Infix:
+( 20 × 9/5 ) + 32 = 68
+
+Postfix:
+20 9 5 / * 32 + = 68
+```
+
+> Worth noting that this calculator handles both integers and floating pint numbers.
+
+### Operators
+
+1. + : Addition
+2. - : Subtraction
+3. * : Multiplication
+4. / : Division
+5. ^ : Exponents
+6. % : Modulo
+7. ! : Factorial
+  - Factorial if the only odd one here, the calcutalor expects atlteast two numbers and an operator, so if you only want to the facotial of one number place a 0 before( although this issue is not present if more than 1 number is given ). Here is an example of finding 10 factorial:
+```
+// Will not work
+> 10 !
+error
+
+// This works
+> 0 10 !
+362880.0
+
+// No 0 needed if other values are present
+> 2 10 ! *
+725760.0
+```
+
+### Examples
+
+Here is a simple example using each operator above
+1 . Addition
+```
+5 2 + = 7
+```
+2. Subtraction
+```
+10 2 - = 8
+```
+3. Multiplication
+```
+3 6 * = 18
+```
+4. Division
+```
+25 5 / = 5
+```
+5. Exponents
+```
+3 2 ^ = 9
+```
+6. Modulo
+```
+10 6 % = 4
+```
+7. Factorial
+```
+0 5 ! = 24
+```
+
+### Use in code
+
+To use Calc.java in another project is quite simple, create with simple contructr and either call the REPL for interactive use, or call with String[] argument for a single calcutalation.
+```
+// Create new Calc object
+Calc calc = new Calc();
+
+// Create String[] to calculate
+String[] tokens = new String[] { "2", "1", "+", "3", "*" };
+
+// Will print out calculation ( "9" in this example )
+System.out.println( calc.run( tokens ));
+
+// Call the interactive Read Eval Print Loop
+calc.repl();
+```
