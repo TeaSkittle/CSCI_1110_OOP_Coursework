d6ceb22127544d9d109b8f3dc90779224f8ce2a1
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-07-06 13:55:11 -0600

    Update Calc.java

--- Final/Calc.java
+++ Final/Calc.java
@@ -65,7 +65,12 @@ public class Calc {
 					    	break;
 				}
 			}
-		} returnValue =  Double.valueOf( stack.pop() );
+		} try {
+		    Double.parseDouble( stack.peek() );
+		    returnValue =  Double.valueOf( stack.pop() );
+		} catch ( NumberFormatException e ) {
+		    System.out.print( "[-]Error: Unknown value " );
+		}
 		stack.clear();
 		return returnValue;
 	}
